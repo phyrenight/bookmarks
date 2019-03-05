9b2d7dd173b5ebfae7767768f3adec04dd7e99f8
Marc Preston <phyrenight@gmail.com>
2019-03-05 17:01:15 -0500

    feature: nothing displayed when a new bookmark is added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 function add_bookmark(){
-    let bookmarkTitle;
-    let bookmarkUrl;
-	bookmarkTitle = document.getElementById("bookmark_title").value
-	bookmarkUrl = document.getElementById("bookmark_url").value
-	console.log(bookmarkTitle)
-	console.log(bookmarkUrl)
-	localStorage.setItem(bookmarkTitle,bookmarkUrl);
-    console.log(localStorage)
+  let bookmarkTitle;
+  let bookmarkUrl;
+  bookmarkTitle = document.getElementById("bookmark_title").value;
+  bookmarkUrl = document.getElementById("bookmark_url").value;
+  if(bookmarkTitle){
+    localStorage.setItem(bookmarkTitle,bookmarkUrl);
+    update_page();
+  }
+  else{
+    document.getElementById("bookmark_title").placeholder = "Please enter a title";
+  }
+}
+
+function update_page(){
+	// if statement is necessary because without it ,it will print methods as well
+	let bookmark;
+	let bookmarksContainer;
+	let n = 0; 
+	for( i in localStorage){
+		if( n < localStorage.length){
+		  bookmarksContainer = document.getElementById("bookmarks");
+          bookmark = "<div><a href='"+localStorage.getItem(i)+"'>"+
+                     i +"</a>";
+          bookmarksContainer.innerHTML += bookmark
+		  console.log(localStorage.getItem(i))
+
+	    }
+	    n++;
+	}
 }
