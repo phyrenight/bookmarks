325126c242b33205dae9e626c16e8d8e0f8ef3ab
Marc Preston <phyrenight@gmail.com>
2019-03-09 16:09:00 -0500

    edit feature not necessary

--- js/main.js
+++ js/main.js
@@ -13,21 +13,42 @@ function add_bookmark(){
 }
 
 function update_page(){
-	// if statement is necessary because without it ,it will print methods as well
-	let bookmark;
-	let bookmarksContainer;
-	let n = 0; 
-	for( i in localStorage){
-		if( n < localStorage.length){
-		  bookmarksContainer = document.getElementById("bookmarks");
-          bookmark = "<div><a href='"+localStorage.getItem(i)+"'>"+
-                     i +"</a></div>";
-          bookmarksContainer.innerHTML += bookmark
-		  console.log(localStorage.getItem(i))
-
-	    }
+  // if statement is necessary because without it ,it will print methods as well
+  let bookmark;
+  let bookmarksContainer;
+  let bookmarkTitle;
+  let n = 0; 
+  for( i in localStorage){
+    if( n < localStorage.length){
+      bookmarksContainer = document.getElementById("bookmarks");
+          bookmarkTitle = i.replace(' ', '_')
+          bookmarkHtml = "<div><a href='"+localStorage.getItem(i)+"'>"+
+                        i +"</a>"+
+                        "<input type='button' value='Delete' id='delete_bookmark_"+bookmarkTitle+"'>"+
+                      "</div>";
+          bookmarksContainer.innerHTML += bookmarkHtml;           
+         function bindButtons(){
+            bookmark = document.getElementById("delete_bookmark_"+bookmarkTitle)
+            bookmark.addEventListener('click', delete_bookmark)
+        }
+      }
 	    n++;
 	}
+  bindButtons
+}
+
+
+function delete_bookmark(){
+  console.log(this)
+  let answer;
+  let bookmarkTitle = this.id.slice(16);
+  bookmarkTitle = bookmarkTitle.replace('_', ' ')
+	answer = confirm("Do you want to delete "+ bookmarkTitle+"?");
+	if(answer){
+    localStorage.removeItem(bookmarkTitle);
+  }console.log(localStorage);
+  console.log(bookmarkTitle);
+  
 }
 
 
