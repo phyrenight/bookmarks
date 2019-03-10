09e2222534edf5a51491a984d8782d768a647364
Marc Preston <phyrenight@gmail.com>
2019-03-09 19:07:33 -0500

    bug: error is thrown when localStorge is empty

--- js/main.js
+++ js/main.js
@@ -20,28 +20,33 @@ function update_page(){
   let bookmarkHtml = "";
   let bookmarkTitle;
   let n = 0; 
-  for( i in localStorage){
-    if( n < localStorage.length){
-      bookmarksContainer = document.getElementById("bookmarks");
-      bookmarkTitle = i.replace(' ', '_')
-      bookmarkHtml += "<div><a href='"+localStorage.getItem(i)+"'>"+
-                      i +"</a>"+
-                      "<input type='button' value='Delete' id='delete_bookmark_"+bookmarkTitle+"'>"+
-                      "</div>";      
+  if(localStorage.length >= 1){
+    for( i in localStorage){
+      if( n < localStorage.length){
+        bookmarksContainer = document.getElementById("bookmarks");
+        bookmarkTitle = i.replace(' ', '_')
+        bookmarkHtml += "<div><a href='"+localStorage.getItem(i)+"'>"+
+                        i +"</a>"+
+                        "<input type='button' value='Delete' id='delete_bookmark_"+bookmarkTitle+"'>"+
+                        "</div>";      
       }
 	    n++;
-	}
-  bookmarksContainer.innerHTML += bookmarkHtml;
-  bindButton();
+	  }
+    bookmarksContainer.innerHTML += bookmarkHtml;
+    bindButton();
+  }
 }
 
 
 function bindButton(){
   function a(){
+    let n = 0;
     for( let i in localStorage){
+      console.log(i)
       bookmarkTitle = i.replace(' ', '_');
       bookmark = document.getElementById("delete_bookmark_"+bookmarkTitle);
       bookmark.addEventListener('click', delete_bookmark);
+      n++;
     }
   }
   a()
